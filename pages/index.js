771de9b95bfcd251610a1619d6ff83b3796651d5
Anderson Cardim Mota <160988546+Anderson-Cardim@users.noreply.github.com>
2026-02-27 20:05:49 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Eu te amo muito muito muito minha Lis!!!</h1>
+  return <h1>Eu te amo muito muito muito minha Lis!!!</h1>;
+}
+
+function teste() {
+  console.log("indenteção errada");
 }
 
 export default Home;
